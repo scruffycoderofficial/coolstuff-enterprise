0b168aed89fb7c535b6316f41657f238eea5be65
Luyanda Siko <sikoluyanda@gmail.com>
2023-08-11 16:24:54 +0200

    Updated Installed and setup JQuery and NProgress at application level.

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -1,13 +1,46 @@
 /**
+ * Globally include NProgress to the application so that it is easy
+ * to have that feature working correctly with Chatify.
+ */
+import NProgress from 'nprogress';
+
+window.NProgress = NProgress;
+
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+try {
+    window.$ = window.jQuery = require('jquery');
+} catch (e) {}
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
 import axios from 'axios';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -17,6 +50,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 import Echo from 'laravel-echo';
 import Pusher from "pusher-js"
 
+window.pusherServiceProvider = process.env.MIX_CHATIFY_PUSHER_SERVICE_PROVIDER;
+
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
@@ -25,7 +60,7 @@ window.Echo = new Echo({
     wsHost: process.env.MIX_PUSHER_HOST,
     wsPort: process.env.MIX_PUSHER_PORT,
     wssPort: process.env.MIX_PUSHER_PORT,
-    authEndpoint: "/broadcasting/auth",
+    authEndpoint: "/messenger/auth",
     forceTLS: false,
     encrypted: true,
     disableStats: true,
