36cde053ac721bee31920354efe3837734159201
Luyanda Siko <sikoluyanda@gmail.com>
2023-09-13 21:24:40 +0200

    All migration files affected by the header annotation adoption

--- src/database/migrations/2023_08_07_999999_add_messenger_color_to_users.php
+++ src/database/migrations/2023_08_07_999999_add_messenger_color_to_users.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ *     This file is part of the CoolStuff IT Solutions package.
+ *
+ *         (c) Luyanda Siko <sikoluyanda@gmail.com>
+ *
+ *     For the full copyright and license information, please view the LICENSE
+ *     file that was distributed with this source code.
+ */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +23,7 @@ class AddMessengerColorToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            if (!Schema::hasColumn('users', 'messenger_color')) {
+            if (! Schema::hasColumn('users', 'messenger_color')) {
                 $table->string('messenger_color')->nullable();
             }
         });
--- src/database/migrations/2023_08_07_999999_create_chatify_messages_table.php
+++ src/database/migrations/2023_08_07_999999_create_chatify_messages_table.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
+/*
+ *     This file is part of the CoolStuff IT Solutions package.
+ *
+ *         (c) Luyanda Siko <sikoluyanda@gmail.com>
+ *
+ *     For the full copyright and license information, please view the LICENSE
+ *     file that was distributed with this source code.
+ */
+
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateChatifyMessagesTable extends Migration
 {
@@ -17,7 +26,7 @@ class CreateChatifyMessagesTable extends Migration
             $table->uuid('id')->primary();
             $table->bigInteger('from_id');
             $table->bigInteger('to_id');
-            $table->string('body',5000)->nullable();
+            $table->string('body', 5000)->nullable();
             $table->string('attachment')->nullable();
             $table->boolean('seen')->default(false);
             $table->timestamps();
