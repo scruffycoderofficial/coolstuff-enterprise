f43a10292d9b3b5d6b28d4d1cdedad17c115e545
Luyanda Siko <sikoluyanda@gmail.com>
2023-09-13 21:09:06 +0200

    Added proper seeds for test Users and generic class header annotation.

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ *     This file is part of the CoolStuff IT Solutions package.
+ *
+ *         (c) Luyanda Siko <sikoluyanda@gmail.com>
+ *
+ *     For the full copyright and license information, please view the LICENSE
+ *     file that was distributed with this source code.
+ */
+
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -16,9 +25,21 @@ class DatabaseSeeder extends Seeder
         User::factory(19)->create();
 
         User::factory()->create([
-             'name' => 'Luyanda Siko',
-             'password' => '@d3!n',
-             'email' => 'sysadmin@coolstuff.com',
+            'name' => 'John Doe',
+            'password' => 'johnd',
+            'email' => 'johnd@doeorg.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Mary Doe',
+            'password' => 'maryd',
+            'email' => 'maryd@doeorg.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Christian Doe',
+            'password' => 'chrisd',
+            'email' => 'chrisd@doeorg.com',
         ]);
     }
 }
