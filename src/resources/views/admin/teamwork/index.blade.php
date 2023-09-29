43b1149486d70e19dadb8f7c0312b49ba7ee2983
Luyanda Siko <sikoluyanda@gmail.com>
2023-09-29 13:27:26 +0200

    Updated teams listing view to show a message when there are no teams available.

--- src/resources/views/admin/teamwork/index.blade.php
+++ src/resources/views/admin/teamwork/index.blade.php
@@ -17,57 +17,61 @@
                                     <i class="fa fa-pencil"></i> Create new
                                 </a>
                             </div>
-                            <table class="table table-striped my-5">
-                                <thead>
-                                <tr>
-                                    <th class="text-primary">Name</th>
-                                    <th class="text-primary">Status</th>
-                                    <th class="text-primary">Manager</th>
-                                    <th></th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($teams as $team)
+                            @if($teams->count() > 0)
+                                <table class="table table-striped my-5">
+                                    <thead>
                                     <tr>
-                                        <td>{{$team->name}}</td>
-                                        <td>
-                                            @if(auth()->user()->isOwnerOfTeam($team))
-                                                <span class="label label-success">Owner</span>
-                                            @else
-                                                <span class="label label-primary">Member</span>
-                                            @endif
-                                        </td>
-                                        <td>{{ $team->team_manager }}</td>
-                                        <td>
-                                            @if(is_null(auth()->user()->currentTeam) || auth()->user()->currentTeam->getKey() !== $team->getKey())
-                                                <a href="{{route('teams.switch', $team)}}" class="btn btn-sm btn-default">
-                                                    <i class="fa fa-sign-in"></i> Switch
-                                                </a>
-                                            @else
-                                                <span class="label label-default">Current team</span>
-                                            @endif
-
-                                            <a href="{{route('teams.members.show', $team)}}" class="btn btn-sm btn-default">
-                                                <i class="fa fa-users"></i> Members
-                                            </a>
-
-                                            @if(auth()->user()->isOwnerOfTeam($team))
-
-                                                <a href="{{route('teams.edit', $team)}}" class="btn btn-sm btn-default">
-                                                    <i class="fa fa-pencil"></i> Edit
-                                                </a>
-
-                                                <form style="display: inline-block;" action="{{route('teams.destroy', $team)}}" method="post">
-                                                    {!! csrf_field() !!}
-                                                    <input type="hidden" name="_method" value="DELETE" />
-                                                    <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i> Delete</button>
-                                                </form>
-                                            @endif
-                                        </td>
+                                        <th class="text-primary">Name</th>
+                                        <th class="text-primary">Status</th>
+                                        <th class="text-primary">Manager</th>
+                                        <th></th>
                                     </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($teams as $team)
+                                        <tr>
+                                            <td>{{$team->name}}</td>
+                                            <td>
+                                                @if(auth()->user()->isOwnerOfTeam($team))
+                                                    <span class="label label-success">Owner</span>
+                                                @else
+                                                    <span class="label label-primary">Member</span>
+                                                @endif
+                                            </td>
+                                            <td>{{ $team->team_manager }}</td>
+                                            <td>
+                                                @if(is_null(auth()->user()->currentTeam) || auth()->user()->currentTeam->getKey() !== $team->getKey())
+                                                    <a href="{{route('teams.switch', $team)}}" class="btn btn-sm btn-default">
+                                                        <i class="fa fa-sign-in"></i> Switch
+                                                    </a>
+                                                @else
+                                                    <span class="label label-default">Current team</span>
+                                                @endif
+
+                                                <a href="{{route('teams.members.show', $team)}}" class="btn btn-sm btn-default">
+                                                    <i class="fa fa-users"></i> Members
+                                                </a>
+
+                                                @if(auth()->user()->isOwnerOfTeam($team))
+
+                                                    <a href="{{route('teams.edit', $team)}}" class="btn btn-sm btn-default">
+                                                        <i class="fa fa-pencil"></i> Edit
+                                                    </a>
+
+                                                    <form style="display: inline-block;" action="{{route('teams.destroy', $team)}}" method="post">
+                                                        {!! csrf_field() !!}
+                                                        <input type="hidden" name="_method" value="DELETE" />
+                                                        <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i> Delete</button>
+                                                    </form>
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            @else
+                                <p>There currently are no Teams available. To add a new Team click on the Create Team call to action on the right-hand corner.</p>
+                            @endif
                         </div>
                     </div>
             </div>
